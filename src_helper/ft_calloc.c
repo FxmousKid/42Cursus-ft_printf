00ca96bc26fca55a5fb761cd20adf9d11302606b
FxmousKid <iyan005@outlook.com>
2024-04-01 23:43:37 +0200

    Adding calloc to helper functions + replaced all mallocs with callocs

---
+++ src_helper/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 02:05:07 by inazaria          #+#    #+#             */
+/*   Updated: 2024/04/01 22:41:53 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/conversions.h"
+
+void	*ft_calloc(size_t total)
+{
+	void	*tab;
+
+	if (total > ULONG_MAX)
+		return (NULL);
+	tab = malloc(total);
+	if (tab == NULL)
+		return (NULL);
+	ft_bzero(tab, total);
+	return (tab);
+}
