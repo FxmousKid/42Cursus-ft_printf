ccf80bb2420a1dbd38499faec853c3d186cba74f
FxmousKid <iyan005@outlook.com>
2024-04-01 23:45:11 +0200

    Adding d and i conversion specifier

---
+++ src_conversions/d_and_i_conversions.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   d_and_i_conversions.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 21:50:27 by inazaria          #+#    #+#             */
+/*   Updated: 2024/04/01 22:40:57 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/conversions.h"
+
+char	*d_and_i_conversions(int num)
+{
+	char	*tab;
+
+	tab = ft_itoa(num);
+	return (tab);
+}
