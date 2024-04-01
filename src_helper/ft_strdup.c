eecdac5b99b38544a7e40c7d8a62a2e0b6f58a53
FxmousKid <iyan005@outlook.com>
2024-04-01 23:43:02 +0200

    Adding ft_strdup to helper functiosn

---
+++ src_helper/ft_strdup.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 22:15:15 by inazaria          #+#    #+#             */
+/*   Updated: 2024/04/01 22:43:43 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/conversions.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	str = (char *) ft_calloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_memcpy(str, s1, len);
+	str[len] = '\0';
+	return (str);
+}
