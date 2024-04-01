fb820759fe1fdd468cf7b3af6497ea832d412aec
FxmousKid <iyan005@outlook.com>
2024-04-01 23:44:57 +0200

    Adding p conversion specifier

---
+++ src_conversions/p_conversion.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   p_conversion.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 23:45:40 by inazaria          #+#    #+#             */
+/*   Updated: 2024/04/01 23:38:08 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/conversions.h"
+#include <stdint.h>
+
+char	*write_nil_str(char *str)
+{
+	char	*tab;
+
+	tab = (char *) ft_calloc(sizeof(char) * 6);
+	if (tab == NULL)
+		return (NULL);
+	str[0] = '(';
+	str[1] = 'n';
+	str[2] = 'i';
+	str[3] = 'l';
+	str[4] = ')';
+	str[5] = 0;
+	return (tab);
+}
+
+void	make_hexa_ptr(char *tab, void *ptr)
+{
+	char		*tmp;
+	uintptr_t	*nb;
+
+	nb = (uintptr_t *) ptr;
+	tab[0] = '0';
+	tab[1] = 'x';
+	tmp = lower_x_conversion((long) nb);
+	printf("tmp: %s\n", tmp);
+	ft_memcpy(tab + 2, tmp, ft_strlen(tmp));
+	tab[ft_strlen(tmp) + 2] = 0;
+	free(tmp);
+}
+
+char	*p_conversion(void *ptr)
+{
+	char	*rendue;
+
+	rendue = (char *) ft_calloc(sizeof(char) * 20);
+	if (rendue == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (write_nil_str(rendue));
+	make_hexa_ptr(rendue, ptr);
+	return (rendue);
+}
