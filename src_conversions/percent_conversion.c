b6f3c1b642c3ebf884af0a2325fd256e5291cbac
FxmousKid <iyan005@outlook.com>
2024-04-02 22:10:27 +0200

    Finished + Tested % conversion

---
+++ src_conversions/percent_conversion.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   percent_conversion.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 21:56:13 by inazaria          #+#    #+#             */
+/*   Updated: 2024/04/02 22:00:38 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/conversions.h"
+
+char	*percent_conversion()
+{
+	char *result;
+
+	result = ft_strdup("%");
+	return (result);
+}
