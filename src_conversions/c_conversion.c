36fcdb23134e7a81519c952220938f0e32cdca2c
FxmousKid <iyan005@outlook.com>
2024-04-02 22:11:01 +0200

    Finished + Tested c convrsion

--- src_conversions/c_conversion.c
+++ src_conversions/c_conversion.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 22:19:52 by inazaria          #+#    #+#             */
-/*   Updated: 2024/04/01 23:33:34 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/02 22:08:27 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ char	*c_conversion(char c)
 {
 	char	*result;
 
-	result = ft_strdup(&c);
+	result = (char *) malloc(sizeof(char) * 2);
 	if (result == NULL)
 		return (NULL);
+	result[0] = c;
+	result[1] = '\0';
 	return (result);
 }
