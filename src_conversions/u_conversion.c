ebdf802a1c617da2fdc53c3b7f08d3a645568b50
FxmousKid <iyan005@outlook.com>
2024-04-03 20:13:06 +0200

    Change u conversion to allow va_arg manipulation

--- src_conversions/u_conversion.c
+++ src_conversions/u_conversion.c
@@ -6,16 +6,17 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 22:02:37 by inazaria          #+#    #+#             */
-/*   Updated: 2024/04/02 22:06:00 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:12:35 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/conversions.h"
 
-char	*u_conversion(unsigned int num)
+va_list	u_conversion(va_list args)
 {
-	char	*str;
+	unsigned int	n;
 
-	str = ft_itoa(num);
-	return (str);
+	n = va_arg(args, unsigned int);
+	ft_putnbr(n);
+	return (args);
 }
