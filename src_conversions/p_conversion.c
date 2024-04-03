63133ba67da576d7639151f39f7343cfc2e8dcbb
FxmousKid <iyan005@outlook.com>
2024-04-03 20:15:08 +0200

    Commiting p conversion wiht malloc, goal is to ft_putchar direclty in conversion without mallocing

--- src_conversions/p_conversion.c
+++ src_conversions/p_conversion.c
@@ -6,27 +6,46 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 23:45:40 by inazaria          #+#    #+#             */
-/*   Updated: 2024/04/02 21:54:57 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:49:19 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/conversions.h"
-#include <stdint.h>
+#include <stdlib.h>
 
-char	*write_nil_str(char *str)
+static int	calc(long num)
 {
-	char	*tab;
+	int	size_malloc;
 
-	tab = (char *) ft_calloc(sizeof(char) * 6);
-	if (tab == NULL)
-		return (NULL);
-	str[0] = '(';
-	str[1] = 'n';
-	str[2] = 'i';
-	str[3] = 'l';
-	str[4] = ')';
-	str[5] = 0;
-	return (tab);
+	size_malloc = 1 + (num < 0);
+	if (num < 0)
+		num = -num;
+	while (num > 15)
+	{
+		size_malloc++;
+		num /= 16;
+	}
+	return (size_malloc);
+}
+
+static	void	add_int_to_string(char *dest, long num, int len, char *base)
+{
+	char	*tmp;
+
+	tmp = dest;
+	if (num < 0)
+	{
+		*tmp++ = '-';
+		len--;
+		num = -num;
+	}
+	tmp[len--] = 0;
+	while (num > 15)
+	{
+		tmp[len--] = base[num % 16];
+		num = num / 16;
+	}
+	tmp[0] = base[num];
 }
 
 void	make_hexa_ptr(char *tab, void *ptr)
@@ -37,21 +56,31 @@ void	make_hexa_ptr(char *tab, void *ptr)
 	nb = (uintptr_t *) ptr;
 	tab[0] = '0';
 	tab[1] = 'x';
-	tmp = lower_x_conversion((long) nb);
+	tmp = (char *) ft_calloc(sizeof(char) * (calc((long) nb) + 1));
+	if (tmp == NULL)
+		return ;
+	add_int_to_string(tmp, (long) nb, calc((long) nb), "0123456789abcdef");
 	ft_memcpy(tab + 2, tmp, ft_strlen(tmp));
 	tab[ft_strlen(tmp) + 2] = 0;
 	free(tmp);
 }
 
-char	*p_conversion(void *ptr)
+va_list	p_conversion(va_list args)
 {
 	char	*rendue;
+	void	*ptr;
 
+	ptr = va_arg(args, void *);
+	if (ptr == NULL)
+	{
+		ft_putstr("(null)");
+		return (args);
+	}	
 	rendue = (char *) ft_calloc(sizeof(char) * 20);
 	if (rendue == NULL)
-		return (NULL);
-	if (ptr == NULL)
-		return (write_nil_str(rendue));
+		return (args);
 	make_hexa_ptr(rendue, ptr);
-	return (rendue);
+	ft_putstr(rendue);
+	free(rendue);
+	return (args);
 }
